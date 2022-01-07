e653174aa14f205dbeedaab10b4af2ea547f6e23
Finley McIlwaine <finleymcilwaine@gmail.com>
2022-01-06 18:01:06 -0700

    make parse error exit with err code in haskell

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,10 +1,11 @@
 module Main where
 
 import ParCPP ( pProgram, myLexer )
+import System.Exit (die)
 
 main :: IO ()
 main = do
   c <- getContents
   case pProgram (myLexer c) of
-    Left err -> putStrLn "parse error!" >> putStrLn err
+    Left err -> putStrLn "parse error!" >> die err
     Right tree -> undefined -- successful parse! typecheck here
